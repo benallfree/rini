908a61ebf0a01cb8c2d152d9c03f64d540638e29
Ben Allfree <ben@benallfree.com>
2021-01-25 07:16:24 -0800

    wip

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,10 +1,46 @@
 import auth from '@react-native-firebase/auth'
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Button, Text, ThemeProvider } from 'react-native-elements'
+import dgram from 'react-native-udp'
 import { Authenticated } from './Authenticated'
 
+function randomPort() {
+  return (Math.random() * 60536) | (0 + 5000) // 60536-65536
+}
+
 const App: FC = () => {
+  useEffect(() => {
+    console.log('useEffect')
+    const socket = dgram.createSocket({ type: 'udp4' })
+    const port = randomPort()
+    const address = '192.168.1.19'
+    socket.bind(port)
+    socket.once('listening', () => {
+      console.log('listening for messages')
+
+      socket.send(
+        'Hello World!',
+        undefined,
+        undefined,
+        41234,
+        address,
+        function (err) {
+          if (err) throw err
+
+          console.log('Message sent!')
+        }
+      )
+    })
+
+    socket.on('message', (msg, rinfo) => {
+      console.log('Message receiveddd', { msg, rinfo })
+    })
+    return () => {
+      socket.close()
+    }
+  }, [])
+
   return (
     <ThemeProvider>
       <View style={styles.container}>
