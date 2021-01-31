e3a665d4ec4a9fed1cfcfedea6253c831d1b2ef1
Ben Allfree <ben@benallfree.com>
2021-01-31 07:03:27 -0800

    wip

--- packages/admin/src/App.tsx
+++ packages/admin/src/App.tsx
@@ -1,4 +1,6 @@
+import firebase from 'firebase'
 import React, { FC } from 'react'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
 import { MapContainer, Marker, TileLayer } from 'react-leaflet'
 import MarkerClusterGroup from 'react-leaflet-markercluster'
 import './App.css'
@@ -6,8 +8,6 @@ import {
   FirebaseAuthProvider,
   FirebaseAuthConsumer,
 } from '@react-firebase/auth'
-import firebase from 'firebase'
-import 'firebase/app'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCQrCIh-R6v-t0Qz-YWrnXRwsn-XhxVQgE',
@@ -20,6 +20,31 @@ const firebaseConfig = {
   measurementId: 'G-WB1XKBDS6G',
 }
 
+const uiConfig: firebaseui.auth.Config = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  callbacks: {
+    signInSuccessWithAuthResult: (authResult) => {
+      console.log('succsss', { authResult })
+      return false
+    },
+  },
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInOptions: [firebase.auth.PhoneAuthProvider.PROVIDER_ID],
+}
+
+const Map: FC = () => {
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <MarkerClusterGroup>
+        <Marker position={[49.8397, 24.0297]} />
+        <Marker position={[52.2297, 21.0122]} />
+        <Marker position={[51.5074, -0.0901]} />
+      </MarkerClusterGroup>
+    </MapContainer>
+  )
+}
 const App: FC = () => {
   return (
     <FirebaseAuthProvider {...firebaseConfig} firebase={firebase}>
@@ -27,24 +52,14 @@ const App: FC = () => {
         <FirebaseAuthConsumer>
           {({ isSignedIn }) => {
             if (isSignedIn === true) {
-              return (
-                <div>
-                  <MapContainer
-                    center={[51.505, -0.09]}
-                    zoom={13}
-                    scrollWheelZoom={false}
-                  >
-                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                    <MarkerClusterGroup>
-                      <Marker position={[49.8397, 24.0297]} />
-                      <Marker position={[52.2297, 21.0122]} />
-                      <Marker position={[51.5074, -0.0901]} />
-                    </MarkerClusterGroup>
-                  </MapContainer>
-                </div>
-              )
+              return <Map />
             }
-            return 'Not signed in'
+            return (
+              <StyledFirebaseAuth
+                uiConfig={uiConfig}
+                firebaseAuth={firebase.auth()}
+              />
+            )
           }}
         </FirebaseAuthConsumer>
       </div>
