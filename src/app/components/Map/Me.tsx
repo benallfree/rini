3041846523f2895f5cde935b942ad40da47a334c
Ben Allfree <ben@benallfree.com>
2021-03-04 14:40:45 -0800

    wip

--- src/app/components/Map/Me.tsx
+++ src/app/components/Map/Me.tsx
@@ -11,7 +11,14 @@ export const Me: FC = () => {
   // console.log('Me', location)
   return (
     <Marker coordinate={location} title={'Me'} description={'My Location'} zIndex={1000}>
-      <Pulse color="blue" numPulses={3} diameter={100} speed={20} duration={2000} />
+      <Pulse
+        color="red"
+        numPulses={3}
+        diameter={100}
+        speed={20}
+        duration={2000}
+        pulseStyle={{ opacity: 0.1 }}
+      />
       <Image source={BundledImages.Tesla} style={{ width: 32, height: 32 }} resizeMode="contain" />
     </Marker>
   )
