56ee2d7dc426cb96b3387fc94dd9c09e654c9f92
Ben Allfree <ben@benallfree.com>
2021-03-16 15:40:09 -0700

    wip

--- src/app/components/Map/Map.tsx
+++ src/app/components/Map/Map.tsx
@@ -1,7 +1,10 @@
+import * as turf from '@turf/turf'
+import { Units } from '@turf/turf'
 import React, { FC, useEffect, useRef } from 'react'
 import { Dimensions, StyleSheet, View } from 'react-native'
 import MapView, { Camera } from 'react-native-maps'
 import { callem } from '../../../callem'
+import { info } from '../../../engine/core/logger'
 import { engine } from '../../engine'
 import { useAppStore } from '../../hooks/store/useAppStore'
 import { Controls } from './Controls'
@@ -10,7 +13,7 @@ import { Others } from './Others'
 import { Overlay } from './Overlay'
 
 const OVERVIEW = { pitch: 0, zoom: 0, altitude: 4000 }
-const DRIVING = { pitch: 50, zoom: 0, altitude: 500 }
+const DRIVING = { pitch: 80, zoom: 0, altitude: 300 }
 
 export const Map: FC = () => {
   const isFollowingRef = useRef(
@@ -40,10 +43,28 @@ export const Map: FC = () => {
   })
 
   useEffect(() => {
-    const unsub = engine.onPlayerMovementChanged((bearing) => {
-      // console.log('position updated', bearing)
-      if (!bearing) return
-      const { latitude, longitude, heading } = bearing
+    const unsub = engine.onPlayerMovementChanged((movement) => {
+      info('position updated', movement)
+      if (!movement) return
+      const { latitude, longitude, heading } = (() => {
+        const { latitude, longitude, heading } = movement
+        // info({ movement })
+        if (mapState.current.mode === 'overview' || !heading) {
+          return movement
+        }
+        const point = turf.point([longitude, latitude])
+        const distance = 1500
+        const bearing = heading
+        const options: { units: Units } = { units: 'feet' }
+        info({ point, distance, bearing, options })
+        const destination = turf.destination(point, distance, bearing, options)
+        info({ destination })
+        return {
+          latitude: destination.geometry.coordinates[1],
+          longitude: destination.geometry.coordinates[0],
+          heading,
+        }
+      })()
       mapState.current.camera = {
         ...mapState.current.camera,
         center: { latitude, longitude },
