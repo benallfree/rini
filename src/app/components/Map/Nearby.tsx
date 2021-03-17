0470d435a013c655f5e0f87bc3d81152366bda65
Ben Allfree <ben@benallfree.com>
2021-03-16 21:10:26 -0700

    wip

--- src/app/components/Map/Nearby.tsx
+++ src/app/components/Map/Nearby.tsx
@@ -1,12 +1,23 @@
 import React, { FC } from 'react'
+import { View } from 'react-native'
 import { Text } from 'react-native-elements'
 import { useNearbyEntityCount } from '../../hooks/store/useNearbyEntityCount'
 
 export const Nearby: FC = () => {
   const count = useNearbyEntityCount()
   return (
-    <Text h3 style={{ color: 'white', textAlign: 'center' }}>
-      {count}
-    </Text>
+    <View>
+      <Text h3 style={{ color: 'white', textAlign: 'center' }}>
+        {count}
+      </Text>
+      <Text
+        style={{
+          fontSize: 8,
+          alignContent: 'center',
+          textAlign: 'center',
+        }}>
+        Nearby
+      </Text>
+    </View>
   )
 }
